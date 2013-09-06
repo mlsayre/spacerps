1df43bf5a926a336b32739abd3f3dea6b3d1fad6
Matt Sayre <mlsayre@gmail.com>
2013-09-06 13:58:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spacerps::Application.config.secret_key_base = '8df2b63d2babc81400f037a569fbd972b5ee2430a5dab379df749c9aa7c8bb6f97acd8dafbfc44a7c05354a84c1f858fd27cb549530852ef1ac1a4be701862f1'
